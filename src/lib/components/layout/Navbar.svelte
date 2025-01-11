3fa795f15e4614d3e22b23d9c1e9249ae42a0fcb
Ethan <ethan.steip@gmail.com>
2025-01-11 12:13:23 -0500

    adding navbar

---
+++ src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Button from '../ui/button/button.svelte';
+	import { Brush, Menu } from 'lucide-svelte';
+</script>
+
+<nav class="flex h-16 w-full items-center px-5 lg:shadow">
+	<div class="w-1/3 sm:w-1/3">
+		<Button variant="ghost" size="icon" href="/">
+			<Brush />
+		</Button>
+	</div>
+	<div class="justify center flex w-1/2 items-center justify-center font-bold sm:w-1/3">
+		<h1 class="tracking-wide">
+			<a href="/">Liz Wyatt Yoga</a>
+		</h1>
+	</div>
+	<div class="flex w-1/3 items-center justify-end sm:w-1/3">
+		<ul class="hidden lg:flex">
+			<li>
+				<Button variant="link">About</Button>
+			</li>
+			<li>
+				<Button variant="link">Pricing</Button>
+			</li>
+			<li>
+				<Button variant="link">Location</Button>
+			</li>
+			<li>
+				<Button variant="link">Lessons</Button>
+			</li>
+		</ul>
+		<Button variant="ghost" size="icon" class="flex lg:hidden">
+			<Menu />
+		</Button>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/layout/Navbar.svelte';
+	// Supports weights 200-800
+	import '@fontsource-variable/manrope';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main>
+	<Navbar />
+	{@render children()}
+</main>
+
+<style>
+	main {
+		font-family: 'Manrope Variable', sans-serif;
+	}
+</style>
